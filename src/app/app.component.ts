16d986722ebe46f7b5e792375304c86f73ee0a65
manssa <msare90@gmail.com>
2020-08-09 14:21:07 +0200

    assignment done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+    .textColor {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
   title = 'angular-course-third-assignment';
+  secret = false;
+  clicks = [];
+
+  onBtnCLicked(): void {
+    this.secret = !this.secret;
+    this.clicks.push('Button clicked ' + new Date());
+    //this.clicks.push(this.clicks.length + 1);
+  }
+
 }
